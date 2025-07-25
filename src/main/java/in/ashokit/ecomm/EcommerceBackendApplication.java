948dbf5b85cf3b27cb646c19066a39b79ed9b4c9
Kishore <sriramdasmani@gmail.com>
2025-07-25 15:38:57 +0530

    1

--- src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
+++ src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
@@ -9,16 +9,6 @@ public class EcommerceBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceBackendApplication.class, args);
 		int i = 10;
-
-		int j = 20;
-
-		String name="ashokit";
-		
-		int k = 30;
-
-		boolean status = false;
-
-		int a = 10;
 		
 	}
 
